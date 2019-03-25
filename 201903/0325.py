8cc94a54eeaf71a9a3a2b23e3d4bd25a510ef044
Xiaopeng WU <shawnwoo@gmail.com>
2019-03-24 17:08:36 -0700

    Update 0325.py

--- 201903/0325.py
+++ 201903/0325.py
@@ -1,2 +1,2 @@
-# Use variables: name = "Daniel's", food = "Sushi", activity = "gaming"
+#2. Use variables: name = "Daniel's", food = "Sushi", activity = "gaming"
 # print out two lines: "Daniel's favorite food is Sushi", "Daniel's favorite activity is gaming!". 

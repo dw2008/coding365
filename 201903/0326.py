d133775bcb426fe4045bee75a77227b9d1547d33
Xiaopeng WU <shawnwoo@gmail.com>
2019-03-25 21:22:39 -0700

    Create 0326.py

---
+++ 201903/0326.py
@@ -0,0 +1,3 @@
+# Daniel can run 3 miles per hour, how long can he ran for half hour? 
+# Daniel has 100 dollars at Jan 1st which is a Sate, he spends 15 dollars everyday. What day will he ran out of money? 
+# Use operaters and variable to calcualte these questions then print it out as following format "Daniel can run {} miles in {} hour. Daniel will ran out of monay on {} day"

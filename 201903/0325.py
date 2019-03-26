54217074fee40fca1238a280e3db0aa0615f20c3
dw2008 <shawnwoo@gmail.com>
2019-03-25 17:59:55 -0700

    Hey my dude I did it :D

--- 201903/0325.py
+++ 201903/0325.py
@@ -1,2 +1,6 @@
 #2. Use variables: name = "Daniel's", food = "Sushi", activity = "gaming"
 # print out two lines: "Daniel's favorite food is Sushi", "Daniel's favorite activity is gaming!". 
+name = "Daniel's"
+food = "sushi"
+activity = "gaming"
+print(f"{name} favorite food is {food}. \n{name} favorite activity is {activity}!")

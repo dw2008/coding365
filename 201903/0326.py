99db275b882508837fbf5ce8c0bfbef415e9ab83
dw2008 <shawnwoo@gmail.com>
2019-03-26 18:39:57 -0700

    Heyo boyo I did the problemo

--- 201903/0326.py
+++ 201903/0326.py
@@ -1,3 +1,15 @@
 # Daniel can run 3 miles per hour, how long can he ran for half hour? 
-# Daniel has 100 dollars at Jan 1st which is a Sate, he spends 15 dollars everyday. What day will he ran out of money? 
+# Daniel has 100 dollars at Jan 1st which is a Sat, he spends 15 dollars everyday. What is the weekday that he will run out of money? 
 # Use operaters and variable to calcualte these questions then print it out as following format "Daniel can run {} miles in {} hour. Daniel will ran out of monay on {} day"
+halfHour = 3 / 2
+print(f"Daniel can run {halfHour} miles in half an hour.")
+amountOfDays = 100 // 15
+dayThatIRunOut = amountOfDays + 1
+aaa = "meh"
+if dayThatIRunOut == 6 :
+ aaa = "friday"
+if dayThatIRunOut == 7 : 
+ aaa = "saturday"
+if dayThatIRunOut == 8 :
+ aaa = "sunday"
+print(f"Daniel will run out of money on {aaa}.")

9368d9f9787147470e87507229cb4b385f2eb5db
Xiaopeng WU <shawnwoo@gmail.com>
2019-03-26 20:57:57 -0700

    Create 0327.py

---
+++ 201903/0327.py
@@ -0,0 +1,5 @@
+#For a given number, if it can be divided by 3 print "this number {} can be divided by 3 ",
+#if it can be divided by 2 print "this number {} can be divided by 2"
+#if it can be divided by 2 and 3, print "this number {} can be divided by 2 and 3"
+#for rest scenario, just print "number {} "
+#Challenge: can you pass in the number from command line instead hard-code it in code? See: https://www.pythonforbeginners.com/argv/more-fun-with-sys-argv

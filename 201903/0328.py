8219fe3d6759f91bac6c1eca761e1162b04413d6
Xiaopeng WU <shawnwoo@gmail.com>
2019-03-27 20:34:27 -0700

    Create 0328.py

---
+++ 201903/0328.py
@@ -0,0 +1,5 @@
+# create a list of String: "apple", "banana", "cherry" print out 
+# 1. Whole list
+# 2. The first element 
+# 3. The last element
+# ref: https://www.w3schools.com/python/python_lists.asp

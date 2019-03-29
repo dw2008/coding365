fd92737007238cb4793c8b22cdf12904a05868ae
Xiaopeng WU <shawnwoo@gmail.com>
2019-03-29 00:22:03 -0700

    Create 0329.py

---
+++ 201903/0329.py
@@ -0,0 +1,3 @@
+# create a list and add 1 to 100, total 100 number in it using for loop(https://www.w3schools.com/python/python_for_loops.asp)
+# print out all numbers in list one by one using for loop
+# print out all numbers reversely using for loop

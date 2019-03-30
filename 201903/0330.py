01c453dc9aabacef8d0163955464832bb8f471fd
Xiaopeng WU <shawnwoo@gmail.com>
2019-03-30 09:28:02 -0700

    Create 0330.py

---
+++ 201903/0330.py
@@ -0,0 +1,4 @@
+#Create an empty list called "all" then add 1 to 100 in it. 
+#Create one list called evenList, another list called oddList. 
+#Loop "all", for each number, if it is even, add to evenList, if it is odd, add to oddList
+#print out eventList and oddList in different line. 

43424836bb6eab66a06e695fa34621eda7f4d157
dw2008 <shawnwoo@gmail.com>
2019-03-29 19:02:37 -0700

    UGHGHGH done

--- 201903/0329.py
+++ 201903/0329.py
@@ -1,3 +1,9 @@
 # create a list and add 1 to 100, total 100 number in it using for loop(https://www.w3schools.com/python/python_for_loops.asp)
 # print out all numbers in list one by one using for loop
 # print out all numbers reversely using for loop
+alist = list()
+for x in range(1, 101):
+  alist.append(x)
+print(alist)
+for x in range(99, 0, -1):
+ 	print(alist[x])

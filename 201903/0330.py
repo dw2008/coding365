ce5acc4a483c731e97a75818daf840db9e3f8f16
dw2008 <tiantianwoah@gmail.com>
2019-03-30 22:20:02 -0700

    dw

--- 201903/0330.py
+++ 201903/0330.py
@@ -1,6 +1,6 @@
 #Your program should able to accepting a number from command line.  
 #if the nubmer is <= 0, return nothing. else do following
-#Create an empty list called "all" then add 1 to this number in it. 
+#Create an empty list called "all" then add 1 to this number in the list. 
 #Create one list called evenList, another list called oddList. 
 #Loop "all", for each number, if it is even, add to evenList, if it is odd, add to oddList
 #print out eventList and oddList in different line. 
@@ -31,3 +31,4 @@ print(oddList)
 print(f"This is a list of all the even numbers before {number}")
 
 print(evenList)
+

f0ed7b4df9745ab4ac065df868741910f07c029d
Xiaopeng WU <shawnwoo@gmail.com>
2019-03-30 23:51:02 -0700

    Create 0331.py

---
+++ 201903/0331.py
@@ -0,0 +1,15 @@
+#Your program should able to accepting a number from command line.  
+
+#if the nubmer is <= 0, return nothing. else do following
+
+#Create an empty list called "all" then add 1 to this number in it. 
+
+#Create one list called evenList, another list called oddList. 
+
+#Loop "all", for each number, if it is even, add to evenList, if it is odd, add to oddList
+
+#print out eventList and oddList in different line. 
+
+#create a function called isEven(x): boolean to verify if a number is even or odd. Refactor your code to use this function. 
+
+#Ref: https://www.w3schools.com/python/python_functions.asp

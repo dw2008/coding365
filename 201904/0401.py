29459e07e8d1e81b19b0e4e25c993d9b9a5e4028
Xiaopeng WU <shawnwoo@gmail.com>
2019-03-31 21:09:11 -0700

    Create 0401.py

---
+++ 201904/0401.py
@@ -0,0 +1,6 @@
+#Given a list of strings {"I", "love", "apple", "I", "don't", "love", "orage"}
+#Manually count the appearance of each string in the list
+#Ref: https://www.tutorialspoint.com/python/python_dictionary.htm
+#Create a dict store the  the count, what is the key and what is the value? 
+#Printout String and its count in follow format in seperate lines "I: 2" "apple: 1"
+#Can you let computer count it for your? 

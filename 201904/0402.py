d9be7000c64339653bf80674f8fa5b9a208e736c
dw2008 <tiantianwoah@gmail.com>
2019-04-02 19:06:44 -0700

    Done now

--- 201904/0402.py
+++ 201904/0402.py
@@ -2,3 +2,12 @@
 #Think about what is the type of x and y
 #Design a func called addString, pass in two single digit numbers represented as string and return its sum in string format as well.
 #Example: addString("1", "2") shoudl return "3"
+def add(x, y) :
+	return x + y
+print(add(1,2))
+
+def addString(x, y) :
+	result = int(x) + int(y) 
+	return str(result)
+
+print(addString("21", "31"))

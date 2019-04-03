7a05cd343291fbfb29557d2132da2f5e321b0907
Xiaopeng WU <shawnwoo@gmail.com>
2019-04-03 08:58:05 -0700

    Rename 0402.py to 0403.py

---
+++ 201904/0403.py
@@ -0,0 +1,3 @@
+#Create two functions:
+# func1: speicalSub(x, y), pass in two integers, if x > y return x - y, if x <=y return 0
+# func2: speicalSub(x, y), pass in two integers, if x or y is 0, return 0, else return x times y

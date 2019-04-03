cd61cc41d1f8cb4e08942fbcb59e5d7e526b0687
Xiaopeng WU <shawnwoo@gmail.com>
2019-04-02 17:45:58 -0700

    Create 0402.py

---
+++ 201904/0402.py
@@ -0,0 +1,4 @@
+#Design a func, pass in two numbers and return its sum "def add(x, y)"
+#Think about what is the type of x and y
+#Design a func called addString, pass in two single digit numbers represented as string and return its sum in string format as well.
+#Example: addString("1", "2") shoudl return "3"

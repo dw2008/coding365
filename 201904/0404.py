ff9519871f5e6018a4b8d6c5a7445983efb982af
Xiaopeng WU <shawnwoo@gmail.com>
2019-04-04 16:20:10 -0700

    Create 0404.py

---
+++ 201904/0404.py
@@ -0,0 +1,2 @@
+#Given a list of numbers, write a function to find the largest number in the list, create another function to find the smallest number 
+#Example: for list{1,2,3, 4, 5,3, 4,2} find largest function should return 5, find smallest function should return 1

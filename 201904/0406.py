86f084679058cdba837cbba0c6d3a6e5ce9c51f8
Xiaopeng WU <shawnwoo@gmail.com>
2019-04-06 10:40:41 -0700

    Create 0406.py

---
+++ 201904/0406.py
@@ -0,0 +1,3 @@
+#Create a function, take list size as input, return a list of random integers(range -1000, 1000).
+#Create a function, take above list as input, return the sum of this list. 
+#Create a function, use above two functions, return the average of the list. 

a8aef8124fb182d9bca26fc72559b5b6dd594886
dw2008 <tiantianwoah@gmail.com>
2019-04-05 18:49:35 -0700

    Doneeeeeeeeeeeeeeeeeee

--- 201904/0405.py
+++ 201904/0405.py
@@ -1,3 +1,22 @@
-#Create a list, put 1000 random interger in it
+#Create a list, put 1000 random integer in it
 #Create a func, find the largest number in the list. 
-#Create anotehr func, find the second largest number in the list. 
+import random
+
+def genRandom(size) :
+	result = list()
+	for x in range(size) :
+		randomNumber = random.randint(-size, size)
+		result.append(randomNumber)
+	return result
+
+def largestNumInList(list) :
+	largestNum = -10000
+	for x in list :
+		if x > largestNum :
+			largestNum = x
+	return largestNum
+
+# print(largestNumInList(genRandom(1000)))
+aList = genRandom(100)
+print(aList)
+print(largestNumInList(aList))

bafe6a8712706cfcd7e2d49dd3fa7784e32e5c99
dw2008 <tiantianwoah@gmail.com>
2019-04-07 19:12:58 -0700

    1273 DOWN THE ROCKEFELLER STREET

--- 201904/0407.py
+++ 201904/0407.py
@@ -1,2 +1,16 @@
 #create a function, inputs are two integers, x and y, returns x ^ y
 #Example, input (2, 2) return 2 ^ 2 = 4
+def powPowPow(x, y) :
+	result = 1
+	if y == 0 :
+		return result
+	for i in range(y) :
+		result = result * x
+	return result
+
+bass = 1234
+power = 1234
+
+
+test1 = powPowPow(bass, power)	
+print(f"{bass} to the power of {power} = {test1}")

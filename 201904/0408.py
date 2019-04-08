5b1935d5d51d3e4c64dcb452e3d45b968d7326b7
Xiaopeng WU <shawnwoo@gmail.com>
2019-04-08 16:04:34 -0700

    Create 0408.py

---
+++ 201904/0408.py
@@ -0,0 +1,2 @@
+#You have the function to calculate POW, can you optimize it so it only needs half of the running time?
+#Hints: 2 * 2 * 2 * 2 = 4 * 4. to calculate 2 ^ 4, you can just calculate 2 ^ 2  then reuse the result to calculate 2 ^ 4 

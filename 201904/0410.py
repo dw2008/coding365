f8edfa7da7b7575bc8b5949be6b53cd4c29b82bb
Xiaopeng WU <shawnwoo@gmail.com>
2019-04-09 21:41:46 -0700

    Create 0410.py

---
+++ 201904/0410.py
@@ -0,0 +1,3 @@
+#Given a list of intergers such as {1,3,4,5,7,8} and a target, such as 5
+#Can you write a function to return the index of the target? 
+#example: for above list, if target is 5 then return 3. (index start from?)

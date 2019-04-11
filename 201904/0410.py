0bcff57bc3586f244f67c72c09dc13d2a1299bce
dw2008 <tiantianwoah@gmail.com>
2019-04-10 19:27:44 -0700

    Uh

--- 201904/0410.py
+++ 201904/0410.py
@@ -1,3 +1,11 @@
 #Given a list of intergers such as {1,3,4,5,7,8} and a target, such as 5
 #Can you write a function to return the index of the target? 
 #example: for above list, if target is 5 then return 3. (index start from?)
+def findIndex(list, target) :
+	for i in range(0, len(list)) :
+		if list[i] == target :
+			return i
+	return -1
+target = 1
+aList = [1, 2, 4, 5, 7, 8]
+print(findIndex(aList, target))

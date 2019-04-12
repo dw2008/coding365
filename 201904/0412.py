f497c5f7a0e32f927e1d28c9cd043272daac17fa
Xiaopeng WU <shawnwoo@gmail.com>
2019-04-11 21:35:58 -0700

    Create 0412.py

---
+++ 201904/0412.py
@@ -0,0 +1,4 @@
+#Create a function.
+#Input: a list of string or integers
+#Output: for element in even index, print out "Even Index: {index number} - Element: {Element value}"
+# for element in odd index, print out "This is Odd: {Element value} "

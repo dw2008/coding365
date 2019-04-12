9531df1ab1f6c590b4d6cae44fe052b94a991fc4
dw2008 <tiantianwoah@gmail.com>
2019-04-11 19:09:18 -0700

    Never gonna give you up Never gonna let you down Never gonna run around and desert you

--- 201904/0411.py
+++ 201904/0411.py
@@ -1,1 +1,10 @@
 #create a func: pass in a list of integers, print out all element in the list in reverse order. 
+def printList(list) :
+	for i in range(0, len(list)) :
+		print(list[i])
+aList = [2, 6, 20, 3, 5, 3]
+print(printList(aList))
+def printListBack(list) :
+	for i in range(len(list) - 1, -1, -1) :
+		print(list[i])
+print(printListBack(aList))

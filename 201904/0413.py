146cd7e9fb9a404d238572f35fc60bbce6eeebb1
dw2008 <tiantianwoah@gmail.com>
2019-04-14 17:51:06 -0700

    A AA AAA

--- 201904/0413.py
+++ 201904/0413.py
@@ -1,3 +1,12 @@
 # create a list and add 1 to 100, total 100 number in it using for loop
 # print out all numbers in list one by one using for loop
 # print out all numbers reversely using for loop
+aList = list()
+for x in range(1, 101) :
+	aList.append(x)
+
+print(aList)
+print(len(aList))
+
+for i in range(len(aList)-1, -1, -1) :
+	print(aList[i])

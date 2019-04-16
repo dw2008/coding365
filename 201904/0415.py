8b0ded9b13e31a18e3bf098ce5733f9f5ac3050a
Xiaopeng WU <shawnwoo@gmail.com>
2019-04-15 18:15:59 -0700

    Create 0415.py

---
+++ 201904/0415.py
@@ -0,0 +1,4 @@
+#Create a function, input two lists of intergers, return a new list which contains larger numbers in each position. 
+#Note: lists in inputs may have different length. 
+#You can use only 1 for loop in your fucntion to generate the new list. 
+#Example: inputs: [1,2,3,4] [3,2,1] outputs: [3,2,3,4]

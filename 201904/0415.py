c705169d7880a375bcdfc854e403e455dd8e9c4d
dw2008 <tiantianwoah@gmail.com>
2019-04-15 18:57:24 -0700

    I got the horses in the back

--- 201904/0415.py
+++ 201904/0415.py
@@ -2,3 +2,19 @@
 #Note: lists in inputs may have different length. 
 #You can use only 1 for loop in your fucntion to generate the new list. 
 #Example: inputs: [1,2,3,4] [3,2,1] outputs: [3,2,3,4]
+numbers1 = [1, 2, 3, 4]
+numbers2 = [3, 2, 1]
+def returnList(alist, blist) :
+	result = list()
+	size = 0
+	if len(alist) < len(blist) :
+		size = len(alist)
+	else :
+		size = len(blist)
+	for i in range(0, size) :
+		if alist[i] > blist[i] :
+			result.append(alist[i])
+		else :
+			result.append(blist[i])
+	return result
+print(returnList(numbers1, numbers2))

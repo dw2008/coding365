173e93cf67dfeba5bd19d97b3e0740ccfe37420a
Daniel Wu <danielwu@Daniels-MacBook-Pro.local>
2019-04-18 19:21:10 -0700

    EEE

--- 201904/0418.py
+++ 201904/0418.py
@@ -2,3 +2,31 @@
 #Create a function, take above list as input, return the sum of this list. 
 #Create a function, use above two functions, return the average of the list.
 #Note: use while loop instead of for to implement 
+import random
+def genRandom(size) :
+    result = list()
+    i = 0
+    while i < size :
+        result.append(random.randint(-1000, 1000))
+        i += 1
+    return result
+
+print(genRandom(10))
+
+def returnSum(alist) :
+    result = 0
+    i = 0
+    while i < len(alist) :
+        result = result + alist[i]
+        i += 1
+    return result
+
+print(returnSum(genRandom(10)))
+
+def returnAvg(alist) :
+    result = 0
+    i = 0
+    sum = returnSum(alist)
+    return sum / len(alist)
+
+print(returnAvg(genRandom(10)))

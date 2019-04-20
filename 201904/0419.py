2b0852635b97c5c344b1ddfc55a98537d97f4490
Xiaopeng WU <shawnwoo@gmail.com>
2019-04-19 17:33:22 -0700

    Update 0419.py

--- 201904/0419.py
+++ 201904/0419.py
@@ -1,11 +1,7 @@
-#Given an array A of integers, return true if and only if it is a valid mountain array.
+#Given an array A of integers, return true if and only if it is sorted in ascending order.
+#
+#sorted means: A[0] < A[1] < ... A[i-1] < A[i]
 
-#Recall that A is a mountain array if and only if:
-
-#A.length >= 3
-#There exists some i with 0 < i < A.length - 1 such that:
-#A[0] < A[1] < ... A[i-1] < A[i]
-#A[i] > A[i+1] > ... > A[B.length - 1]
  
 
 #Example 1:
@@ -15,9 +11,9 @@
 #Example 2:
 
 #Input: [3,5,5]
-#Output: false
+#Output: true
 #Example 3:
 
 #Input: [0,3,2,1]
-#Output: true
+#Output: false
  

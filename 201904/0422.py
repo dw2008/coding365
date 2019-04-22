253f04188f2abc805adbecbbd1683192b37457a0
Xiaopeng WU <shawnwoo@gmail.com>
2019-04-22 15:09:22 -0700

    Create 0422.py

---
+++ 201904/0422.py
@@ -0,0 +1,3 @@
+#Given a list of integers (sorted in decending order) and a target
+#Use binary search to find the index of the target. If target does not exist, return -1.
+#Print out how many loops did you use until find the target or quite the program

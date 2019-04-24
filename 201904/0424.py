fe372f804d3520fbf378a126f5143aa7895850bf
Xiaopeng WU <shawnwoo@gmail.com>
2019-04-24 10:39:23 -0700

    Create 0424.py

---
+++ 201904/0424.py
@@ -0,0 +1,4 @@
+#Given an ordered list which is sorted in descending order and a target. 
+#Return the position in the list if target exist in the list
+#If target does not exist in the list, return the position this target should be inserted to. 
+#Example: [4, 2, 1] target = 3, return 1(the third position in the list) since 3 should be inserted before 2 and after 4.

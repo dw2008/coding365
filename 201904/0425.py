a6ada43ab6c38feea552a5cfc68126e656ce93e7
Xiaopeng WU <shawnwoo@gmail.com>
2019-04-25 18:50:45 -0700

    Create 0425.py

---
+++ 201904/0425.py
@@ -0,0 +1,5 @@
+#Implement a function to print out how many of each vowel are present in an input string. 
+#Example: 
+#Input: "Apple"
+#Output: "Total number of As: 1, Total number of Es: 1, Total number of Is: 0, Total Number of Os: 0, Total number of Us: 0"
+#Note: upper and lower case should be counted together. 

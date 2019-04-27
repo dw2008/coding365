b6c776a2ee750efef69d9b003803f97a7f909ac1
Daniel Wu <danielwu@Daniels-MacBook-Pro.local>
2019-04-26 19:57:18 -0700

    -Golf ball goes into lake- Wii golf: NICE SHOT

--- 201904/0426.py
+++ 201904/0426.py
@@ -1,3 +1,9 @@
 #Create a 2D array https://www.geeksforgeeks.org/python-using-2d-arrays-lists-the-right-way/
 #Set border elements to 1 and other elements to 0.
 #Print out this 2D array.
+rows, cols = (5, 5) 
+arr = [[0]*cols]*rows 
+arr[0][4] = 1
+arr[0][0] = 1
+for row in arr: 
+    print(row) 

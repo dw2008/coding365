61683b6eaaa8a3069a1e12021f160a45374ca01f
Xiaopeng WU <shawnwoo@gmail.com>
2019-04-28 20:04:07 -0700

    Create 0428.py

---
+++ 201904/0428.py
@@ -0,0 +1,5 @@
+#Implement a function to return a 2D array which filled with 0s. 
+
+#Implement another function takes above 2D array as input and mark its border as 1 then return it. 
+
+#Printout this 2D array

6e0f21566f756f93e30c30fbe0c4e8a8204b48f3
dw2008 <danielwu@Daniels-MacBook-Pro.local>
2019-04-29 19:14:10 -0700

    turtle

--- 201904/0429.py
+++ 201904/0429.py
@@ -1,3 +1,11 @@
 #https://docs.python.org/3.3/library/turtle.html?highlight=turtle
 #https://www.geeksforgeeks.org/turtle-programming-python/
 #Write a function to draw a turtle and let it move forward
+import turtle
+turtle.bgcolor("grey")
+turtle.speed(0)
+colors = ["red", "blue", "yellow", "green"]
+for x in range(360) :
+    turtle.pencolor( colors[ x % 4 ])
+    turtle.circle(x)
+    turtle.left(90)

dd9c148090ce727002a93759685693eab7b3322b
Xiaopeng WU <shawnwoo@gmail.com>
2019-04-30 16:11:35 -0700

    Update 0430.py

--- 201904/0430.py
+++ 201904/0430.py
@@ -1,2 +1,3 @@
 #use turtle to draw a Olympic ring*
 #Following methods in turtle library need to be used: pensize, color, speed, forward, backward, circle, penup, pendown
+#Color: black: pen.color("butts"). Same syntax for "red", "yellow", "green", "blue"

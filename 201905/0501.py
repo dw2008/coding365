d9c85fd6f3411dbec6e31269ac9b09f3daad0af9
dw2008 <danielwu@Daniels-MacBook-Pro.local>
2019-05-01 19:10:26 -0700

    Yeetus Deletus

--- 201905/0501.py
+++ 201905/0501.py
@@ -1,2 +1,17 @@
 #Finish half of this tutorial https://hourofpython.trinket.io/a-visual-introduction-to-python#/welcome/an-hour-of-code
 #Write your own code below
+import turtle
+yeetus = turtle.Turtle()
+deletus = turtle.Turtle()
+
+yeetus.color("yellow")
+yeetus.backward(100)
+
+deletus.color("red")
+deletus.forward(100)
+
+yeetus.write("Yeetus")
+deletus.write("Deletus")
+
+yeetus.forward(100)
+deletus.backward(100)

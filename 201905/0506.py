9593e92eef5433f6a34c06900e7a106bdf7c0a34
dw2008 <danielwu@Daniels-MacBook-Pro.local>
2019-05-07 19:38:47 -0700

    $.$

--- 201905/0506.py
+++ 201905/0506.py
@@ -3,62 +3,29 @@
 #After the game, print out player's name and its score.
 import turtle
 import random
-
 turt = turtle.Turtle()
-alist = ["Yeet", "Yet"]
-name = raw_input("What's your name? ")
-score = 0
 
-turt.penup()
-turt.goto(0, 200)
-number = random.randint(0, 1)
-numberb = alist[number]
+riddles = [
+["I come out of a cold morning's sigh", "And your freshly baked pie", "I am alive when I die", "And can arise from your chai"],
+["An expert weaver", "Yet so small", "So many patterns", "But that's not all"],
+["AAA"]
+]
 
-if numberb == "Yeet" :
-    riddleA()
-    turt.goto(0, 0)
-    riddleB()
 
-elif numberb == "Yet" :
-    riddleB()
-    turt.goto(0, 0)
-    riddleA()
-
-turt.goto(-200, 100)
-turt.write(name, "'s score was :", font=("Arial", 16, "normal"))
-turt.goto(-200, 0)
-turt.write(score, font=("Arial", 16, "normal"))
-
-win=turtle.Screen()
-win.exitonclick()  
-
-def line(words, horiz_pos = -50):
-    x,y = turt.pos()
+def line(words, horiz_pos = -50) :
+    x, y = turt.pos()
     turt.goto(max(horiz_pos, -190), y)
     turt.write(words, font=("Arial", 16, "normal"))
-    x,y = turt.pos()
+    x, y = turt.pos()
     turt.goto(x, y - 25)
-
-def riddleA() :
-    line("I come out of a cold morning's sigh")
-    line("And your freshly baked pie")
-    line("I am alive when I die")
-    line("And can arise from your chai")
-    aRiddle = raw_input("What's the answer? (Lowercase, please) ")
-    print(aRiddle)
-    if aRiddle == "steam" :
-        score = score + 1
-        turt.write("Noice", font=("Arial", 16, "normal"))
-    else :
-        turt.write("No", font=("Arial", 16, "normal"))
-def riddleB() :
-    line("An expert weaver")
-    line("Yet so small")
-    line("So many patterns")
-    line("But that's not all")
-    aRiddle = raw_input("What's the answer? (Lowercase, please) ")
-    print(aRiddle)
-    if aRiddle == "spider" :
-        turt.write("Noice", font=("Arial", 16, "normal"))
-    else :
-        turt.write("No", font=("Arial", 16, "normal"))
+def initScreen() :
+    turt.penup()
+    turt.goto(0, 200)
+def printRiddle(riddle) :
+    for x in riddle:
+        line(x)
+    
+initScreen()
+printRiddle(riddles[0])
+win=turtle.Screen()
+win.exitonclick()

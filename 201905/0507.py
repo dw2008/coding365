0648e3154a006c499eb4d354b80d75231e90a3d7
dw2008 <danielwu@Daniels-MacBook-Pro.local>
2019-05-07 19:42:11 -0700

    $.$

--- 201905/0507.py
+++ 201905/0507.py
@@ -1,1 +1,29 @@
 #Refactor your code to make it more modulize and easy to reuse
+import turtle
+import random
+turt = turtle.Turtle()
+
+riddles = [
+["I come out of a cold morning's sigh", "And your freshly baked pie", "I am alive when I die", "And can arise from your chai"],
+["An expert weaver", "Yet so small", "So many patterns", "But that's not all"],
+["AAA"]
+]
+
+
+def line(words, horiz_pos = -50) :
+    x, y = turt.pos()
+    turt.goto(max(horiz_pos, -190), y)
+    turt.write(words, font=("Arial", 16, "normal"))
+    x, y = turt.pos()
+    turt.goto(x, y - 25)
+def initScreen() :
+    turt.penup()
+    turt.goto(0, 200)
+def printRiddle(riddle) :
+    for x in riddle:
+        line(x)
+    
+initScreen()
+printRiddle(riddles[0])
+win=turtle.Screen()
+win.exitonclick()

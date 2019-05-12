f3acfff41e9dd49cb14f226a90780cedebe297d0
Xiaopeng WU <shawnwoo@gmail.com>
2019-05-11 18:35:53 -0700

    Create 0511.py

---
+++ 201905/0511.py
@@ -0,0 +1,3 @@
+# review binary search problem
+# Given a list of ascending sorted number and a target
+# Write a function to return: if target is in the list, return the index of target, otherwise return the insertion point

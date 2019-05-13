b92005a6363a83100f08ed5d28b783d91d7819fb
dw2008 <danielwu@Daniels-MacBook-Pro.local>
2019-05-12 20:18:28 -0700

    It's free real estate

--- 201905/0512.py
+++ 201905/0512.py
@@ -1,1 +1,11 @@
 #Create a function to draw a ring, take the input as color
+import turtle
+turt = turtle.Turtle()
+color = "green"
+def drawRing(acolor) :
+    turt.color(acolor)
+    turt.pensize(40)
+    turt.circle(100)
+drawRing(color)
+win=turtle.Screen()
+win.exitonclick()

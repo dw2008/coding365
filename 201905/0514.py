74bcd99e1d4909200a301d02b90522146e638239
dw2008 <danielwu@Daniels-MacBook-Pro.local>
2019-05-14 19:21:16 -0700

    Text: I can't. Email: I cannot. 4000 word essay: Henceforth, I am unable to can.

--- 201905/0514.py
+++ 201905/0514.py
@@ -1,2 +1,19 @@
 #Write a function to take a paragraph of words as input
 #Use turtle to printout the whole paragraph and print out how many words in this paragraph. 
+import turtle
+turt = turtle.Turtle()
+paragraph = raw_input("Paragraph ")
+turt.penup()
+turt.goto(-200, 175)
+turt.write(paragraph, font=("Arial", 16, "normal"))
+def numberOfWords(aparagraph) :
+    result = 0
+    bparagraph = aparagraph.split(" ")
+    for x in bparagraph :
+        result += 1 
+    return result
+ree = numberOfWords(paragraph)
+turt.goto(-200, 140)
+turt.write(ree, font=("Arial", 16, "normal"))
+win=turtle.Screen()
+win.exitonclick()

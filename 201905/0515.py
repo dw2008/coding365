221f0f779753dd5417e3228d384910c667de482a
Xiaopeng WU <shawnwoo@gmail.com>
2019-05-15 09:17:21 -0700

    Create 0515.py

---
+++ 201905/0515.py
@@ -0,0 +1,3 @@
+#A paragraph of words contains some non-character symbols, can you write a function to clean up them then return the readable paragraph? 
+#Example: input - "Tod2ay i2-s S()unday." output: "Today is Sunday."
+#Hint: Google "python check if character is letter"

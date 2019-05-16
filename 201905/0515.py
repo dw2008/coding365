c69536fbedacc91264da8118a5fe2bf72b3b9132
dw2008 <danielwu@Daniels-MacBook-Pro.local>
2019-05-15 19:13:53 -0700

    A leak of Detective Pikachu was just him dancing for over 2 hours

--- 201905/0515.py
+++ 201905/0515.py
@@ -1,3 +1,12 @@
 #A paragraph of words contains some non-character symbols, can you write a function to clean up them then return the readable paragraph? 
 #Example: input - "Tod2ay i2-s S()unday." output: "Today is Sunday."
 #Hint: Google "python check if character is letter"
+def weedOut(aparagraph) :
+    result = ""
+    for char in aparagraph:
+        if char.isalpha() == True or char == ' ' or char == '.' or char == ',' or char == '!' or char == '?':
+            result = result + char
+    return result
+
+paragraph = "Th3eref0or3e, I d0o n0ot w4ant t0o d0o th1is."
+print(weedOut(paragraph))

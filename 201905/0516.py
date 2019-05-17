ca2aec0ab9ecf5cb9e202216e48976ad04237656
dw2008 <danielwu@Daniels-MacBook-Pro.local>
2019-05-16 18:53:05 -0700

    UnoRIgInAl

--- 201905/0516.py
+++ 201905/0516.py
@@ -1,2 +1,7 @@
 #Write two functions: one called encrypt which reverse all words in a paragrah, another called decrypt which reverse them back. 
 #Example: "I love pizza" -> "I evol azzip" -> "I love pizza"
+someWords = raw_input("")
+def encrypt(words) :
+    return words[::-1]
+print(encrypt(someWords))
+print(encrypt(encrypt(someWords)))

2c68e3a3342da5deae80f62a25f96f0f89c810d9
Xiaopeng WU <shawnwoo@gmail.com>
2019-05-16 18:30:14 -0700

    Create 0516.py

---
+++ 201905/0516.py
@@ -0,0 +1,2 @@
+#Write two functions: one called encrypt which reverse all words in a paragrah, another called decrypt which reverse them back. 
+#Example: "I love pizza" -> "I evol azzip" -> "I love pizza"

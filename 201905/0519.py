285853c5827b6b3b1eb7dea21c27a82eeb2700b7
dw2008 <danielwu@Daniels-MacBook-Pro.local>
2019-05-19 18:09:18 -0700

    Dang it autocorrect RESTORATION 100

--- 201905/0519.py
+++ 201905/0519.py
@@ -1,2 +1,12 @@
 #Use recursive function to calculate factorial: https://en.wikipedia.org/wiki/Factorial
 #Example: input 1, output 1; input 2, output 2; input 3, output 6...
+number = input("Number ")
+def factorial(number) :
+    if number == 0 :
+        return 1
+    else :
+        anumber = 1
+        for x in range(1, number + 1) :
+            anumber = anumber * x
+        return anumber
+print(factorial(number))

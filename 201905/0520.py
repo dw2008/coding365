f464370f1bfa76ff8947d8ce9e21fffa453e4562
dw2008 <danielwu@Daniels-MacBook-Pro.local>
2019-05-20 19:40:07 -0700

    UHhHHhhhHhhhh

--- 201905/0520.py
+++ 201905/0520.py
@@ -1,7 +1,5 @@
 #Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
 
- 
-
 #Example 1:
 
 #Input: "Hello"
@@ -14,3 +12,10 @@
 
 #Input: "LOVELY"
 #Output: "lovely"
+def lowercase(aword) :
+    result = ""
+    for x in aword :
+        result = result + x.lower()
+    return result
+word = raw_input("Word ")
+print(lowercase(word))

f7b22f8fb767666512160b8e9cc023b2bcb05a4f
dw2008 <danielwu@Daniels-MacBook-Pro.local>
2019-05-21 19:34:00 -0700

    Press F to pay respects

--- 201905/0521.py
+++ 201905/0521.py
@@ -1,3 +1,13 @@
 #Input: two string
 #Output: Common unique characters of these two strings
 #Example: "bella" , "else". output: "e", "l"  
+def commonChar(aword, bword) :
+    result = list()
+    for x in aword :
+        for y in bword :
+            if x == y :
+                result.append(x)
+    return result
+worda = raw_input("Word ")
+wordb = raw_input("Another word ")
+print(commonChar(worda, wordb))

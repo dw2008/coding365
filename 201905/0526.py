c48ab40cdd4188fec4c88790cdfefc2d0fb23d1c
dw2008 <danielwu@Daniels-MacBook-Pro.local>
2019-05-26 18:53:17 -0700

    Memer: Small brain Memist: Normal-sized brain Memisseur: Bigger brain Unemployed: Massive brain

--- 201905/0526.py
+++ 201905/0526.py
@@ -2,3 +2,4 @@
 Learn what is a big O notaion from https://www.youtube.com/watch?v=D6xkbGLQesk&t=46s
 What is the big O of bubble sort?
 """
+print("It's the relationship between the input and the steps required to complete the algorithm")

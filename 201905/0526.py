3b736ca268138e4c3db6d08992d8a92275f3c1b0
dw2008 <danielwu@Daniels-MacBook-Pro.local>
2019-05-27 19:55:57 -0700

    aaaaaAAAAAAAAAUUUUUGHHHH

--- 201905/0526.py
+++ 201905/0526.py
@@ -2,4 +2,13 @@
 Learn what is a big O notaion from https://www.youtube.com/watch?v=D6xkbGLQesk&t=46s
 What is the big O of bubble sort?
 """
-print("It's the relationship between the input and the steps required to complete the algorithm")
+# It's the relationship between the input and the steps required to complete the algorithm
+def bubbleSort(alist) :
+    number = len(alist)
+    for i in range(number) :
+        for j in range(0, number-1-i) :
+            if alist[j] > alist[j + 1] :
+                alist[j], alist[j + 1] = alist[j + 1], alist[j]
+alist = [4, 5, 2, 6, 8]
+bubbleSort(alist)
+print(alist)
--- 201905/0527.py
+++ 201905/0527.py
@@ -2,3 +2,14 @@
 Search for "insertion sort" and implement it in python. 
 What is the complexity of this algorithm? 
 """
+def insertionSort(alist, target, swap) :
+    result = list()
+    for x in alist :
+        if x == target :
+            result.append(swap)
+        else :
+            result.append(x)
+    return result
+alist = [0, 5, 3, 2, 66, 2, 2, 2, 2]
+print(insertionSort(alist, 2, 3))
+print("The insertion sort's complexity is N^2.")

37a9273bff4528bf8b1ddff41de0f612d1b66de8
dw2008 <danielwu@Daniels-MacBook-Pro.local>
2019-05-30 19:08:04 -0700

    I GOT THE HORSES IN THE BACK

--- 201905/0530.py
+++ 201905/0530.py
@@ -1,3 +1,14 @@
 """
 change your bubble sort algorithm to sort the list in reversing order. 
 """
+def bubbleSort(alist) :
+    number = len(alist)
+    counter = 0
+    for i in range(number) :
+        for j in range(0, number-1-i) :
+            if alist[j] < alist[j + 1] :
+                alist[j], alist[j + 1] = alist[j + 1], alist[j]
+            counter += 1
+alist = [9, 2, 5, 1, 42]
+bubbleSort(alist)
+print(alist)

3ada6c094b20b86cabc409c4b29ce6d30b24c675
dw2008 <danielwu@Daniels-MacBook-Pro.local>
2019-06-01 20:50:54 -0700

    Fudgesicles

--- 201906/0601.py
+++ 201906/0601.py
@@ -6,3 +6,16 @@ Input: "2 * 6" return " 2 * 6 = 12"
 
 Note: your function should support "+, -, *, /". If input string has more than one operator, just return an error message "only support one operator"
 """
+def mathEquation(one, two, operator) :
+    if operator == "+" :
+        return one + two
+    elif operator == "-" :
+        return one - two
+    elif operator == "*" :
+        return one * two
+    elif operator == "/" :
+        return one / two
+    else :
+        return "What the hecc are you inputing"
+operator = raw_input("Operator ")
+print(mathEquation(1.1, 2.2, operator))

086fcda2ba310d9bc910912bae16fc80ffdf17f0
Xiaopeng WU <shawnwoo@gmail.com>
2019-06-01 18:04:42 -0700

    Create 0601.py

---
+++ 201906/0601.py
@@ -0,0 +1,8 @@
+"""
+Write a function which can interperate a string to a math equation then print out the calculation result
+
+Input: "1 + 2 " return "1 + 2 = 3"
+Input: "2 * 6" return " 2 * 6 = 12"
+
+Note: your function should support "+, -, *, /". If input string has more than one operator, just return an error message "only support one operator"
+"""
